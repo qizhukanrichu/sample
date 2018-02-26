a14a4ff7639c74b3e14cb69db75fd020fd0ebaff
Yuan xin <2150919412@qq.com>
2018-02-26 09:23:21 +0000

    Finish layout and routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
+
+//注册路由时，URI signup 和 /signup 从使用上来看，并无区别，Laravel 框架兼容这两种写法。
+Route::get('signup','UsersController@create')->name('signup');
